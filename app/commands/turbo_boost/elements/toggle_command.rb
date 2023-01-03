fedae2192e14686e095fbb13a6b1f3e7ee0a2268
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-01-03 07:45:38 -0700

    Setup pattern matching to validate element input

--- app/commands/turbo_boost/elements/toggle_command.rb
+++ app/commands/turbo_boost/elements/toggle_command.rb
@@ -4,6 +4,8 @@ class TurboBoost::Elements::ToggleCommand < TurboBoost::Elements::ApplicationCom
   prevent_controller_action
 
   def show
+    return unless valid_element?
+
     if element.remember?
       state[element.aria.controls] = true
     else
@@ -14,11 +16,45 @@ class TurboBoost::Elements::ToggleCommand < TurboBoost::Elements::ApplicationCom
   end
 
   def hide
+    return unless valid_element?
     state[element.aria.controls] = false
     morph id: element.morphs, html: render(element.render_options)
   end
 
   def toggle
+    return unless valid_element?
     element.aria.expanded? ? hide : show
+  end
+
+  private
+
+  def element_errors
+    @element_errors ||= Set.new
+  end
+
+  def valid_element?
+    valid = validate_element_attributes && validate_element_aria_attributes
+    element_errors.each { |error| streams << turbo_stream.invoke("console.error", args: [error]) }
+    valid
+  end
+
+  def validate_element_attributes
+    case element
+    in {renders: _, morphs: _} then return true
+    in {renders: _} then element_errors << "The trigger element is missing the `morphs` attribute!"
+    in {morphs: _} then element_errors << "The trigger element is missing the `renders` attribute!"
+    else element_errors << "The trigger element is missing the `renders` and `moprhs` attributes!"
+    end
+    false
+  end
+
+  def validate_element_aria_attributes
+    case element.aria
+    in {controls: _, expanded: _} then return true
+    in {controls: _} then element_errors << "The trigger element is missing the `aria-expanded` attribute!"
+    in {expanded: _} then element_errors << "The trigger element is missing the `aria-controls` attribute!"
+    else element_errors << "The trigger element is missing the `aria-controls` and `aria-expanded` attributes!"
+    end
+    false
   end
 end
