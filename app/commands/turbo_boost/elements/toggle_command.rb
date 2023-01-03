9bd97a9858e4a1cf052bf20b36f00b4220f2181a
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-01-03 09:36:47 -0700

    Raise on invalid element for toggle trigger

--- app/commands/turbo_boost/elements/toggle_command.rb
+++ app/commands/turbo_boost/elements/toggle_command.rb
@@ -4,7 +4,7 @@ class TurboBoost::Elements::ToggleCommand < TurboBoost::Elements::ApplicationCom
   prevent_controller_action
 
   def show
-    return unless valid_element?
+    validate_element!
 
     if element.remember?
       state[element.aria.controls] = true
@@ -16,44 +16,38 @@ class TurboBoost::Elements::ToggleCommand < TurboBoost::Elements::ApplicationCom
   end
 
   def hide
-    return unless valid_element?
+    validate_element!
     state[element.aria.controls] = false
     morph id: element.morphs, html: render(element.render_options)
   end
 
   def toggle
-    return unless valid_element?
+    validate_element!
     element.aria.expanded? ? hide : show
   end
 
   private
 
-  def element_errors
-    @element_errors ||= Set.new
+  def validate_element!
+    validate_element_attributes! && validate_element_aria_attributes!
   end
 
-  def valid_element?
-    valid = validate_element_attributes && validate_element_aria_attributes
-    element_errors.each { |error| streams << turbo_stream.invoke("console.error", args: [error]) }
-    valid
-  end
-
-  def validate_element_attributes
+  def validate_element_attributes!
     case element
     in {renders: _, morphs: _} then return true
-    in {renders: _} then element_errors << "The trigger element is missing the `morphs` attribute!"
-    in {morphs: _} then element_errors << "The trigger element is missing the `renders` attribute!"
-    else element_errors << "The trigger element is missing the `renders` and `moprhs` attributes!"
+    in {renders: _} then raise TurboBoost::Commands::InvalidElementError, "The trigger element is missing the `morphs` attribute!"
+    in {morphs: _} then raise TurboBoost::Commands::InvalidElementError, "The trigger element is missing the `renders` attribute!"
+    else raise TurboBoost::Commands::InvalidCommandError, "The trigger element is missing the `renders` and `moprhs` attributes!"
     end
     false
   end
 
-  def validate_element_aria_attributes
+  def validate_element_aria_attributes!
     case element.aria
     in {controls: _, expanded: _} then return true
-    in {controls: _} then element_errors << "The trigger element is missing the `aria-expanded` attribute!"
-    in {expanded: _} then element_errors << "The trigger element is missing the `aria-controls` attribute!"
-    else element_errors << "The trigger element is missing the `aria-controls` and `aria-expanded` attributes!"
+    in {controls: _} then raise TurboBoost::Commands::InvalidElementError, "The trigger element is missing the `aria-expanded` attribute!"
+    in {expanded: _} then raise TurboBoost::Commands::InvalidElementError, "The trigger element is missing the `aria-controls` attribute!"
+    else raise TurboBoost::Commands::InvalidElementError, "The trigger element is missing the `aria-controls` and `aria-expanded` attributes!"
     end
     false
   end
