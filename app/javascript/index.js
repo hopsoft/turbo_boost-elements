760f10acc594cdb4c2022aa97fa097b705b0068b
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-02-15 08:29:12 -0700

    Add version and move commands to be a peer dependency

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,9 +1,10 @@
 import '@turbo-boost/commands'
 import devtools from '@turbo-boost/devtools'
+import VERSION from './version'
 import './elements'
 
 self.TurboBoost = self.TurboBoost || {}
 self.TurboBoost.devtools = devtools
-self.TurboBoost.Elements = {}
+self.TurboBoost.Elements = { VERSION }
 
 export default self.TurboBoost.Elements
